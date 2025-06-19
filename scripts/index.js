9219ff9deaadbf347bf5786f6f3d782bfb86d373
Cyrus <okikiolacyrus@gmail.com>
2025-06-19 23:17:58 +0100

    Creates char and animes dropdown list

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,88 @@ import {animes, charas} from './data.js';
 
 let light = true;
 let currChar = 1;
+let timeouts = [];
+let intervals = [];
 
 siteInitializer();
 
 
-function siteInitializer(animeID, charID){
-    let startAnime = (animeID ? animeID : animes[0]);
+function siteInitializer(animeInd, charInd){
+    clearAllTimeoutsAndIntervals();
+    console.log(animeInd, charInd);
+    let startAnime = (animeInd ? animes[animeInd] : animes[0]);
+    console.log(startAnime);
     const animeNameTag = document.querySelector('.anime-name');
     const charName = document.querySelector('.char-name');
     const charDesc = document.querySelector('.char-desc');
     const charIMG = document.querySelector('.char-img-js');
     animeNameTag.textContent = startAnime.name;
+    dropdownGenerator(startAnime.id);
     charas.find(chara =>{
-        console.log('looking for chara');
-        if(chara.id == (charID? charID : startAnime.chars[0])){
+        //Charind is touching the wrong thing. We're meant to get the index in anime.chars. not charas
+        if(chara.id == (charInd ? startAnime.chars[charInd] : startAnime.chars[0])){
             charName.textContent = chara.name;
-            charDesc.textContent = chara.desc
+            charDesc.textContent = chara.desc;
             charIMG.src = chara.charIMGs[0];
             modeHandler(chara.modeName);
             charAction(chara.id);
         }
     });
+}
+
+function dropdownGenerator(animeID){
+    console.log('dgenerator active');
+    console.log(animeID);
+    const animesListTag = document.querySelector('.animes-list-js');
+    const charsListTag = document.querySelector('.chars-list-js');
+    let charsListHTML = ``;
+    let animeListHTML = ``;
+    animes.forEach((anime, index)=>{
+        animeListHTML += `
+            <li class="select-list-item">
+                <a href="#" data-anime-ind="${index}" class="select-link anime-link">${anime.name}
+                </a>
+            </li>
+        `;
+    })
+    if(animeID){
+        animes.find((anime, aindex)=>{
+            if(anime.id == animeID){
+                anime.chars.forEach(charID=>{
+                    charas.find(chara=>{
+                        if(chara.id == charID){
+                            let cindex;
+                            anime.chars.find((id, ind)=>{
+                                if(chara.id == id){
+                                    cindex = ind;
+                                    console.log(`cindex of ${chara.name} in animes.char is ${cindex}`);
+                                    //cindex is now the index in animes.char
+                                }
+                            })
+                            console.log(`found, ${chara}, ${chara.name}, ${cindex}`);
+                            charsListHTML += `
+                                <li class="select-list-item"><a href="#" data-anime-ind="${aindex}" data-char-ind="${cindex}" class="select-link char-link">${chara.name}</a></li>
+                            `;
+                        } 
+                    })
+                })
+            }
+        })
+    }
+    animesListTag.innerHTML = animeListHTML;
+    charsListTag.innerHTML = charsListHTML;
+    document.querySelectorAll('.char-link').forEach(link=>{
+        link.addEventListener('click', (e)=>{
+            console.log('char clicked');
+            siteInitializer(e.target.dataset.animeInd, e.target.dataset.charInd);
+        });
+    })
+    document.querySelectorAll('.anime-link').forEach(link=>{
+        link.addEventListener('click', (e)=>{
+            console.log('anime clicked.');
+            siteInitializer(e.target.dataset.animeInd);
+        });
+    })
 }
 
 function modeHandler(mode){
@@ -46,26 +107,27 @@ function charAction(currCharInd){
     charas.find(chara =>{
         if(chara.id == currCharInd){
             char = chara;
-            console.log(chara);
         }
     })
     imgsLength = char.charIMGs.length;
-    setInterval(()=>{
+    let intervalID = setInterval(()=>{
         if(currIndex == imgsLength){
             currIndex = 0;
         }
         changeCharIMG(char.charIMGs[currIndex]);
         currIndex+=1;
     }, 6000);
+    intervals.push(intervalID);
 }
 
 function changeCharIMG(nextCharImg){
     let imgTag = document.querySelector('.char-img-js');
     imgTag.style.animation ="fade-slide-away-left 1s ease-in-out forwards";
-    setTimeout(()=>{
+    let timeoutID = setTimeout(()=>{
         imgTag.src = nextCharImg;
         imgTag.style.animation ="fade-slide-left 1s ease-in-out forwards";
     },2000);
+    timeouts.push(timeoutID);
 }
 
 function toggleMode(){
@@ -76,6 +138,16 @@ function toggleMode(){
         document.body.classList.remove('dark-mode');
         light = true;
     }
+}
+
+function clearAllTimeoutsAndIntervals(){
+    timeouts.forEach(timeout =>{
+        clearTimeout(timeout);
+    })
+
+    intervals.forEach(interval=>{
+        clearInterval(interval);
+    })
 }
 
 //Make characters of an anime change periodically.
